e05494b482aced11de92c8e3a9904b4184def7e8
Moriand <32554375+Moriand@users.noreply.github.com>
2020-05-24 17:00:11 -0500

    Update intervalos.R

--- Hipotesis/intervalos.R
+++ Hipotesis/intervalos.R
@@ -1,15 +1,16 @@
 intervalo = function(ds,n,x,u){
   tmp = c(u-2*ds/sqrt(n),u+2*ds/sqrt(n))
   if (tmp[1] > x){
-    txt = paste(" == Alarma limite inferior == \n la media x =",x," es menor que",tmp[1])
+    txt = paste(" ---== Alarma limite inferior ==--- \n la media x =",x," es menor que",tmp[1])
     txt1 = paste(txt, "\n",tmp[1],"<",x,"<",tmp[2])
-    writeLines(txt1) 
+    cat(txt1) 
   }else if(tmp[2]< x){
-    txt = paste("== Alarma  limite superior== \n"," x = ",x, " es mayor que ",tmp[2])
-   txt1 = paste(txt, "\n",tmp[1],"<",x,"<",tmp[2])
-    writeLines(txt1)
-  }
+    txt = paste(" ---== Alarma  limite superior ==--- \n"," x = ",x, " es mayor que ",tmp[2])
+   txt1 = paste(txt, "\n \n",tmp[1],"<",x,"<",tmp[2])
+    cat(txt1)
+  }else{
     # inferior ,  superior
-  return(tmp)
+    return(tmp)
+  }
 }
 # cuando dice Alarma, significa que la media "X" no esta entre los límites calculados y eso es un problema para cálculos
